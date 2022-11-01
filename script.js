0042f420340e3d44455b09be19a65a2628fa9bcd
Gabriel Caravantes <107145434+gabrielcaravantes@users.noreply.github.com>
2022-11-01 00:59:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const passInput = document.querySelector("#inputPasswordId");
-const lenInput = document.querySelector("#inputLenghtId");
+const lenInput = document.querySelector("#inputLengthId");
 const infoLength = document.querySelector('label[for="inputLengthId"]');
 const btnGerar = document.querySelector("#btnGerar");
 
-const chkLowe = document.querySelector("#chkLowerId");
+const chkLower = document.querySelector("#chkLowerId");
 const chkUpper = document.querySelector("#chkUpperId");
 const chkNumber = document.querySelector("#chkNumberId");
 const chkSymbols = document.querySelector("#chkSymbolsId");
@@ -11,5 +11,22 @@ const chkSymbols = document.querySelector("#chkSymbolsId");
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const symbols = ["!", "@", "#", "$", "%"];
 
-const caracters = Array.from(Array(26).map((_, i) => i + 97));
+const caracters = Array.from(Array(26)).map((_, i) => i + 97);
 const LowercaseCaracters = caracters.map((item) => String.fromCharCode(item));
+const UppercaseCaracters = LowercaseCaracters.map((item) => item.toUpperCase());
+
+infoLength.innerHTML = lenInput.value;
+
+lenInput.addEventListener("change", () => {
+  infoLength.innerHTML = lenInput.value;
+});
+
+btnGerar.addEventListener("click", () => {
+  generatePassword(
+    chkNumber.checked,
+    chkSymbols.checked,
+    chkLower.checked,
+    chkUpper.checked,
+    lenInput.value
+  );
+});
